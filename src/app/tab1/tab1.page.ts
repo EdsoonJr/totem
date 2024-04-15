6cc9912b04207dde0b12af6ee1a82e58a2d6cd32
Edson Junior <edsonjn2013@gmail.com>
2024-04-15 00:22:37 -0300

    create totem

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  ultimaSenhaGerada: { senha: string, tipo: string, icon: string } = { senha: '', tipo: '', icon: '' };
 
-  constructor() {}
+  constructor(public senhasService: SenhasService) {}
 
+  gerarSenha(tipo: string) {
+    const senha = this.senhasService.gerarSenha(tipo);
+    const senhaInfo = { senha, tipo, icon: this.senhasService.getIcon(tipo) };
+    
+    this.ultimaSenhaGerada = senhaInfo;
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SenhasService } from '../services/senhas.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  constructor(public senhasService: SenhasService) {}
 
-  constructor() {}
+  // Métodos para calcular os relatórios
+  calcularQuantitativoGeralEmitidas(): number {
+    return this.senhasService.senhas.length;
+  }
 
+  calcularQuantitativoGeralAtendidas(): number {
+    return this.senhasService.ultimasSenhasChamadas.length;
+  }
+
+  calcularQuantitativoPorPrioridade(tipo: string): number {
+    return this.senhasService.senhas.filter(senha => senha.icon === this.senhasService.getIcon(tipo)).length;
+  }
+
+  calcularQuantitativoAtendidasPorPrioridade(tipo: string): number {
+    return this.senhasService.ultimasSenhasChamadas.filter(senha => senha.icon === this.senhasService.getIcon(tipo)).length;
+  }
 }
